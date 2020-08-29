66c50a7f608b1d56687d3464b11e9567efa24bae
Tijs Martens <tijs@bothrs.com>
2020-08-29 21:24:21 +0200

    dynamix redirect url

--- src/components/Authorize.svelte
+++ src/components/Authorize.svelte
@@ -1,6 +1,9 @@
 <script>
   const SPOTIFY_CLIENT_ID = "4b712dd1511c4e6381691f9f9a994254";
-  const authorize_link = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=token&redirect_uri=https://spotify-recommendation-engine.vercel.app&scope=user-top-read%20user-read-private%20user-read-email%20user-library-read`;
+  const authorize_link = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=token&redirect_uri=${window.location.href}&scope=user-top-read%20user-read-private%20user-read-email%20user-library-read`;
+
+
+  console.log(window.location.href)
 
   const params = new URLSearchParams(window.location.href);
   if (window.location.href.split("#access_token=")[1]) {
